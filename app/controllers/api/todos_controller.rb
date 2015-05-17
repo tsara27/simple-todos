f0a3a6905ab355b66ddec6cf0e2d1d771c66ea1b
tsara27 <tsara.tf@gmail.com>
2015-05-17 20:43:39 +0700

    remove unused method

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,7 +1,15 @@
-class TodosController < ApiController
+class Api::TodosController < ApiController
 
-  def index
-    
+
+  def create
+    todo = Todo.create!(todo_params)
+    render json: todo.to_json, status: 200
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:text).merge(status: params[:status])
   end
 
 end
