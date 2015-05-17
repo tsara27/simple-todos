e10a78d483ec120ea61a6c71eda26f5cd212622c
tsara27 <tsara.tf@gmail.com>
2015-05-17 10:13:38 +0700

    add root path and api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  namespace :api do
+    resources :todos
+  end
 
   # Example resource route with options:
   #   resources :products do
