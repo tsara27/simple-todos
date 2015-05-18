ad919207d5233843fca3d8d51d566a40bcdf8926
tsara27 <tsara.tf@gmail.com>
2015-05-18 16:10:32 +0700

    add function

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,9 +1,20 @@
-function on_form_submit() {
-  $('#todo_form').bind('ajax:success', function(data, status, xhr) {
-    $.get( "/home", function(html) {
-      html_response = $.parseHTML(html);
-      console.log(html);
-      $(".todos").html(html);
-    });
+function get_home_html() {
+  $.get( "/home", function(html) {
+    html_response = $.parseHTML(html);
+    console.log(html);
+    $(".todos").html(html);
   });
 };
+
+function on_form_submit() {
+  $('#todo_form').bind('ajax:success', function(data, status, xhr) {
+    get_home_html();
+  });
+};
+
+function move_to_doing() {
+  $('.move_to').bind('ajax:success', function(data, status, xhr) {
+    get_home_html();
+  });
+};
+
