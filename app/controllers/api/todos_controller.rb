51b5746d4b6f662f0e0908168f447ec6b0bf8950
tsara27 <tsara.tf@gmail.com>
2015-05-18 16:11:12 +0700

    modify status and add update method

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -2,6 +2,13 @@ class Api::TodosController < ApiController
 
   def create
     todo = Todo.create!(todo_params)
+    render json: todo.to_json, status: 201
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.status = params[:status]
+    todo.save!
     render json: todo.to_json, status: 200
   end
 
