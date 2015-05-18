7b4a1ee09df7512fd132777c5b2828caf6916030
tsara27 <tsara.tf@gmail.com>
2015-05-18 14:52:12 +0700

    add presenter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @listed = Todo.where(status: "listed")
+    @home_presenter = HomePresenter.new
+    render locals: { listed: @home_presenter.listed, doing: @home_presenter.doing, done: @home_presenter.done }
   end
 
-  def method_name
-    
-  end
 end
---
+++ app/presenters/home_presenter.rb
@@ -0,0 +1,15 @@
+class HomePresenter
+
+  def listed
+    Todo.listed
+  end
+
+  def doing
+    Todo.doing
+  end
+
+  def done
+    Todo.done
+  end
+  
+end
